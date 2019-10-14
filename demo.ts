bd6dd7ac72a9e2dd115eb831ea5cdfa6a59556bb
start-2code <44686142+start-2code@users.noreply.github.com>
2019-10-14 14:23:24 +0100

    added enum

--- demo.ts
+++ demo.ts
@@ -3,8 +3,51 @@
 //% weight=10 color=#008B00 icon="\uf136" block="matest"
 namespace matest {
 
+    export enum aMotors {
+        //% blockId="M1" block="M1"
+        M1 = 0,
+        //% blockId="M2" block="M2"
+        M2 = 1,
+        //% blockId="All" block="All"
+        All = 2
+    }
 
-    /% weight=90
+    export enum aServos {
+        //% blockId="S1" block="S1"
+        S1 = 0,
+        //% blockId="S2" block="S2"
+        S2 = 1
+    }
+
+    export enum Dir {
+        //% blockId="CW" block="CW"
+        CW = 0x0,
+        //% blockId="CCW" block="CCW"
+        CCW = 0x1
+    }
+
+    export enum Patrol {
+        //% blockId="PatrolLeft" block="PatrolLeft"
+        PatrolLeft = 13,
+        //% blockId="PatrolRight" block="PatrolRight"
+        PatrolRight = 14
+    }
+
+    export enum LED {
+        //% blockId="LEDLeft" block="LEDLeft"
+        LEDLeft = 8,
+        //% blockId="LEDRight" block="LEDRight"
+        LEDRight = 12
+    }
+
+    export enum LEDswitch {
+        //% blockId="turnOn" block="turnOn"
+        turnOn = 0x01,
+        //% blockId="turnOff" block="turnOff"
+        turnOff = 0x00
+    }
+    
+    //% weight=90
     //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed"
     //% speed.min=0 speed.max=255
     //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
