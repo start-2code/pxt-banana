15e668910f392ee01382892c214c608c7da5c923
start-2code <ickhatib@gmail.com>
2019-10-14 14:36:33 +0100

    new function

--- demo.ts
+++ demo.ts
@@ -57,4 +57,13 @@ namespace matest {
 
     }
 
+    //% weight=100
+    //% blockId=motor_MotorRun2 block="Motor2|%index|dir|%Dir|speed|%speed"
+    //% speed.min=0 speed.max=255
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
+    export function MotorRun2(index: aMotors, direction: Dir, speed: number): void {
+
+
+    }
 }
