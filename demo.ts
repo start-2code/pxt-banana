1c603e8b89f19a15f50606d76e7b1b3516e828ff
start-2code <44686142+start-2code@users.noreply.github.com>
2019-10-14 14:21:27 +0100

    new

---
+++ demo.ts
@@ -0,0 +1,17 @@
+// Add your code here
+
+//% weight=10 color=#008B00 icon="\uf136" block="matest"
+namespace matest {
+
+
+    /% weight=90
+    //% blockId=motor_MotorRun block="Motor|%index|dir|%Dir|speed|%speed"
+    //% speed.min=0 speed.max=255
+    //% index.fieldEditor="gridpicker" index.fieldOptions.columns=2
+    //% direction.fieldEditor="gridpicker" direction.fieldOptions.columns=2
+    export function MotorRun(index: aMotors, direction: Dir, speed: number): void {
+
+
+    }
+
+}
